bfef3eb07067932021296088acfe07e8154833ef
doublejuan1124 <2282424838@qq.com>
2018-09-10 19:38:34 +0800

    提交

---
+++ blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from blog.models import Article
+
+# Register your models here.
+
+
+
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ('title','pub_date')
+
+
+
+admin.site.register(Article,ArticleAdmin)
